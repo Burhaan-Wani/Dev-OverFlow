1ea399e1e20a8fee9676a1d979e7fcded4c13ed3
Burhaan <burhaanwani823@gmail.com>
2024-07-30 16:21:40 +0530

    added meta data and fonts

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,7 +2,12 @@ import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const page = () => {
-    return <UserButton />;
+    return (
+        <>
+            <UserButton />
+            <h1 className="h1-bold">hi there</h1>
+        </>
+    );
 };
 
 export default page;
