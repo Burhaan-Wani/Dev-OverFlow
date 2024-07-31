600ee1e9d474e299ef3d0a62d631f68fb83f3022
Burhaan <burhaanwani823@gmail.com>
2024-07-31 16:32:45 +0530

    added right sidebar

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,15 +1,62 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import RenderTag from "./RenderTag";
 // import RenderTag from "./RenderTag";
 
 const RightSidebar = () => {
+    const hotQuestions = [
+        // give me 5 questions having an id, title
+        {
+            _id: "6176855482672486402",
+            title: "How to create a new React project?",
+        },
+        {
+            _id: "6176855482672486403",
+            title: "How to create a new Angular project?",
+        },
+        {
+            _id: "6176855482672486404",
+            title: "How to create a new Rext project?",
+        },
+        {
+            _id: "6176855482672486405",
+            title: "How to create a new Remix project?",
+        },
+        {
+            _id: "6176855482672486406",
+            title: "How to create a new Gatsby project?",
+        },
+    ];
+
+    const popularTags = [
+        {
+            _id: "6176855482672486404",
+            name: "javascript",
+            numberOfQuestions: 2,
+        },
+        {
+            _id: "6176855482672486403",
+            name: "react",
+            numberOfQuestions: 4,
+        },
+        {
+            _id: "6176855482672486402",
+            name: "nextjs",
+            numberOfQuestions: 2,
+        },
+        {
+            _id: "6176855482672486401",
+            name: "vite",
+            numberOfQuestions: 2,
+        },
+    ];
     return (
         <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
             <div>
                 <h3 className="h3-bold text-dark200_light900">Top Questions</h3>
                 <div className="mt-7 flex w-full flex-col gap-[30px]">
-                    {/* {hotQuestions.map((question) => (
+                    {hotQuestions.map((question) => (
                         <Link
                             href={`/question/${question._id}`}
                             key={question._id}
@@ -26,13 +73,13 @@ const RightSidebar = () => {
                                 className="invert-colors"
                             />
                         </Link>
-                    ))} */}
+                    ))}
                 </div>
             </div>
             <div className="mt-16">
                 <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
                 <div className="mt-7 flex flex-col gap-4">
-                    {/* {popularTags.map((tag) => (
+                    {popularTags.map((tag) => (
                         <RenderTag
                             key={tag._id}
                             _id={tag._id}
@@ -40,7 +87,7 @@ const RightSidebar = () => {
                             totalQuestions={tag.numberOfQuestions}
                             showCount
                         />
-                    ))} */}
+                    ))}
                 </div>
             </div>
         </section>
