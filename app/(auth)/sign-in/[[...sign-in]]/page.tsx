c2b4c13ba1e76d7af0bad2ca351d45c593dbccc4
Burhaan <burhaanwani823@gmail.com>
2024-08-05 12:44:28 +0530

    bugfix

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 import React from "react";
 
 const page = () => {
-    return <SignIn fallbackRedirectUrl={"/"} />;
+    return <SignIn />;
 };
 
 export default page;
