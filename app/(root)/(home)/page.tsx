b9b75ec22c07584dd99b8da1bf01216b8ced1958
Burhaan <burhaanwani823@gmail.com>
2024-08-05 12:02:47 +0530

    fixed some bugs

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -10,6 +10,7 @@ import Link from "next/link";
 import React from "react";
 
 const Home = async () => {
+    console.log("hi there");
     const result = await getQuestions({});
     return (
         <>
